093312dd7d54c14f6c468c033221f8d3ca5f3151
Bohdan Onsha <onsha.bogdan@gmail.com>
2020-12-01 13:21:07 +0200

    Configure django admindocs.

--- employee_management/employee_management/urls.py
+++ employee_management/employee_management/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
     path('', include('department_app.urls')),
 ]
